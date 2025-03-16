4373308da05215f11c1656fa0296d10e5c6d1281
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-16 18:30:51 +0100

    feat: add global colors

---
+++ design/colors.ts
@@ -0,0 +1,10 @@
+export enum Colors {
+  white = '#fff',
+  black = '#000',
+  background = '#F5F5F5',
+  blue = 'rgb(10, 132, 255)',
+  error = 'rgb(255, 59, 48)',
+  grey1 = '#5a5a5a',
+  grey2 = '#8c8b8b',
+  grey3 = '#ccc',
+}
