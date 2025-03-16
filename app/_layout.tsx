4018d8a072820ce326c650e66f367ce81ca31842
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-16 23:55:26 +0100

    feat: configure stack screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { useTranslation } from '@/hooks/useTranslation';
+
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 
 export default function RootLayout() {
-  return <Stack />;
+  const t = useTranslation();
+  const colorScheme = useColorScheme();
+
+  const statusBarStyle = colorScheme !== 'dark' ? 'light' : 'dark';
+
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: t('hotelList.title') }} />
+        <Stack.Screen
+          name="hotel"
+          options={{ title: t('hotelDetails.title'), headerShown: false }}
+        />
+      </Stack>
+      <StatusBar style={statusBarStyle} />
+    </>
+  );
 }
