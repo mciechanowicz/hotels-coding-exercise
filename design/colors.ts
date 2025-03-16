e3fe29f6069d599b11c441f7fa5b600f8610087f
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-16 19:43:15 +0100

    feat: add new colors and translations

--- design/colors.ts
+++ design/colors.ts
@@ -3,6 +3,8 @@ export enum Colors {
   black = '#000',
   background = '#F5F5F5',
   blue = 'rgb(10, 132, 255)',
+  green = '#4CAF50',
+  red = '#FF6B6B',
   error = 'rgb(255, 59, 48)',
   grey1 = '#5a5a5a',
   grey2 = '#8c8b8b',
