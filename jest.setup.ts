b9042fdf496547e0bf328b5429e94cf77449a1c2
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-18 14:24:39 +0100

    feat: setup global mocks for translation and icons

---
+++ jest.setup.ts
@@ -0,0 +1,12 @@
+import { Scope, TranslateOptions } from 'i18n-js';
+
+jest.mock('@/hooks/useTranslation', () => ({
+  useTranslation:
+    () =>
+    (scope: Scope, _options?: TranslateOptions): string =>
+      scope as string,
+}));
+
+jest.mock('@expo/vector-icons', () => ({
+  FontAwesome: () => null,
+}));
