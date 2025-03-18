4766b262aabe4edf15e1114f678a9f16a3fa9ee7
Michał <michal.ciechanowicz@stermedia.eu>
2025-03-19 00:45:12 +0100

    refactor: change status bar style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,13 @@
 import { useTranslation } from '@/hooks/useTranslation';
-
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useColorScheme } from 'react-native';
 
 export default function RootLayout() {
   const t = useTranslation();
-  const colorScheme = useColorScheme();
-
-  const statusBarStyle = colorScheme !== 'dark' ? 'light' : 'dark';
 
   return (
     <>
+      <StatusBar style="dark" />
       <Stack>
         <Stack.Screen name="index" options={{ title: t('hotelList.title') }} />
         <Stack.Screen
@@ -19,7 +15,6 @@ export default function RootLayout() {
           options={{ title: t('hotelDetails.title'), headerShown: false }}
         />
       </Stack>
-      <StatusBar style={statusBarStyle} />
     </>
   );
 }
